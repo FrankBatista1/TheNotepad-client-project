c2a2747251e3aee6f3abb06c7ac36cb3c865fec9
FrankBatista1 <fb53901@gmail.com>
2021-09-19 13:16:20 -0400

    Every alert whit no errors

--- src/views/FieldsPage.js
+++ src/views/FieldsPage.js
@@ -1,60 +1,62 @@
 import Card from "../components/Card";
 import { useState, useEffect } from "react";
 import { getFieldsFromApi } from "../services/fieldservices";
-import { deleteFieldFromApi} from "../services/fieldservices"
-import Swal from 'sweetalert2'
-import '../stylesheets/Deleteimage.css'
+import { deleteFieldFromApi } from "../services/fieldservices";
+import Swal from "sweetalert2";
+import "../stylesheets/Deleteimage.css";
 
-
- const FieldsPage = () => {
+const FieldsPage = () => {
   const [fields, setFields] = useState([]);
- 
 
   useEffect(() => {
     getFields();
-  }, [])
+  }, []);
 
   const getFields = async () => {
     try {
       const response = await getFieldsFromApi();
-      console.log(response)
+      console.log(response);
       setFields(response.data);
     } catch (error) {
-      console.log("Server not working")
+      console.log("Server not working");
     }
-  }
+  };
 
   const handleDelete = async (id) => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "You won't be able to revert this!",
-      icon: 'warning',
+      icon: "warning",
       iconColor: "#d10a0a",
       showCancelButton: true,
       confirmButtonColor: "#878787",
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it'
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it",
     }).then((result) => {
-        if (result.isConfirmed) {
-        const filtered = fields.filter((field) => {
-          return field._id !== id
-        }).then(() => deleteFieldFromApi(id).then(() => setFields(filtered))).then(() => Swal.fire('Deleted!','Your field has been deleted.','success'))
-        
+      if (result.isConfirmed) {
+        const filtered = fields
+          .filter((field) => field._id !== id)
+          deleteFieldFromApi(id)
+          setFields(filtered)
+          Swal.fire("Deleted!", "Your field has been deleted.", "success");
       }
-    })
-   
+    });
   };
 
-
   return (
-    
-    <div style={{marginTop: '50px', marginBottom: '50px'}}>
-       {fields && fields.map(field => (
+    <div style={{ marginTop: "50px", marginBottom: "50px" }}>
+      {fields &&
+        fields.map((field) => (
           <div key={field._id}>
-            <Card props={field}/><img onClick={(event) => handleDelete(field._id)} className='deletebutton' src="https://www.freeiconspng.com/thumbs/x-png/red-x-png-4.png" alt="x"></img>
+            <Card props={field} />
+            <img
+              onClick={(event) => handleDelete(field._id)}
+              className="deletebutton"
+              src="https://www.freeiconspng.com/thumbs/x-png/red-x-png-4.png"
+              alt="x"
+            ></img>
           </div>
         ))}
-
     </div>
   );
 };
